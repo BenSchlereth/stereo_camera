a4fbaa9743c8c4705726f98d18ee8fd319df4d7e
Benedikt Schlereth <schlerbt@iis.fraunhofer.de>
2020-04-03 07:31:39 +0200

    test with shape-detection

--- cone-detection.py
+++ cone-detection.py
@@ -7,7 +7,7 @@ def canny(image):
     """returns only the edges"""
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
-    canny = cv2.Canny(blur, 50, 100)
+    canny = cv2.Canny(blur, 20, 20)
     return canny
 
 #def template():
@@ -26,42 +26,32 @@ def colorfilter(image):
     res = cv2.bitwise_and(hsv, hsv, mask=mask)
 
     # GaussianBlur with (x,x) is the Pixelsize and will determinate the viewing distance
-    blur = cv2.GaussianBlur(res, (5,5), 0)
-    return blur, hsv
+    blur = cv2.GaussianBlur(res, (15,15), 0)
+    return blur, mask
 
 
 
 image = cv2.imread('Messungen/Messung_1/Gerade_Links.jpg')
+template = cv2.imread('Messungen/templates/Green-Cone_2.png')
 lane_image = np.copy(image)
-filtered_image, converted_image = colorfilter(image)
-img1 = cv2.cvtColor(filtered_image, cv2.COLOR_RGB2GRAY)
+filtered_image, green_mask = colorfilter(lane_image)
+gray_image = cv2.cvtColor(filtered_image, cv2.COLOR_RGB2GRAY)
+gray_template = cv2.cvtColor(template, cv2.COLOR_RGB2GRAY)
 
-#template not working properly
-img2 = cv2.imread('Messungen/templates/Green-Cone_1.png', cv2.IMREAD_GRAYSCALE) # trainImage
-# Initiate SIFT detector
-sift = cv2.xfeatures2d.SIFT_create()
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
-# BFMatcher with default params
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2,k=2)
-# Apply ratio test
-good = []
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
-# cv.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-plt.imshow(img3),plt.show()
+# Threshold image
+_,im_match = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY)
+_,template_match = cv2.threshold(gray_template, 128, 255, cv2.THRESH_BINARY)
 
+d2 = cv2.matchShapes(im_match,template_match,cv2.CONTOURS_MATCH_I2,0)
+print(d2)
 
+canny_image = canny(filtered_image)
 
 cv2.namedWindow("original",cv2.WINDOW_NORMAL)
 cv2.resizeWindow("original", 600,600)
-cv2.imshow("original", template)
+cv2.imshow("original", lane_image)
 cv2.namedWindow("result",cv2.WINDOW_NORMAL)
 cv2.resizeWindow("result", 600,600)
-cv2.imshow("result", filtered_image)
+cv2.imshow("result", canny_image)
 
 cv2.waitKey(0)
