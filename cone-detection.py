00694b2d0c091155f6e7ed9e407405f07c9186fd
Benedikt Schlereth <schlerbt@iis.fraunhofer.de>
2020-06-16 10:01:08 +0200

    distance estimation for each cone

--- cone-detection.py
+++ cone-detection.py
@@ -65,35 +65,51 @@ for con in hull:
     ratio = box[2] / box[3]
     if 0.6 < ratio < 0.8:
         detected = Cone()
-        detected.top_box = box
-        detected.top_con = con
+        detected.upper_box = box
+        detected.upper_con = con
         cones.append(detected)
 
 # find the bottom part of a cone
 for cone in cones:
-    min_distance = 1000
+    min_distance = -1
     for con in hull:
         bounding_box = cv2.boundingRect(con)
         is_below = False
-        if (bounding_box[1] - cone.top_box[1]) > 0:
+        if (bounding_box[1] - cone.upper_box[1]) > 0:
             is_below = True
-        distance = np.sqrt((bounding_box[1] - cone.top_box[1])**2 + (bounding_box[0] - cone.top_box[0])**2)
-        if min_distance > distance > 0 and is_below:
+        distance = np.sqrt((bounding_box[1] - cone.upper_box[1]) ** 2 + (bounding_box[0] - cone.upper_box[0]) ** 2)
+        if min_distance > distance > 0 and is_below or min_distance == -1:
             min_distance = distance
             possible_box = bounding_box
             possible_con = con
-    print("estimation with ratio", (min_distance / cone.top_box[3] - 1.57) / 0.06, "m")
-    print("estimation with height", (2 * 229) / cone.top_box[3], "m")
-    y_Koo = possible_box[1] + possible_box[3] - cone.top_box[1]
-    print("estimation with overall-height", (2 * 708) / y_Koo, "m")
-    print()
-    cone.bottom_box = possible_box
-    cone.bottom_con = possible_con
+    # print("estimation with ratio", (min_distance / cone.upper_box[3] - 1.57) / 0.06, "m")
+    # print("estimation with height", (2 * 229) / cone.upper_box[3], "m")
+    # y_Koo = possible_box[1] + possible_box[3] - cone.upper_box[1]
+    # print("estimation with overall-height", (2 * 708) / y_Koo, "m")
+    # print()
+    cone.lower_box = possible_box
+    cone.lower_con = possible_con
 
 # draw bounding boxes
 for cone in cones:
-    cone.bounding_box()
-    cv2.rectangle(image, cone.left_bottom_corner, cone.right_top_corner, (0, 0, 255), 10)
+    # cone.bounding_box()
+    cone.upper_bounding_box()
+    cone.lower_bounding_box()
+    cone.distance()
+    print("right", cone.lower_right_bottom)
+    print("left", cone.lower_left_bottom)
+    print()
+    x1 = cone.upper_right_bottom[0]
+    y1 = cone.upper_right_bottom[1]
+    x2 = cone.upper_right_top[0]
+    y2 = cone.upper_right_top[1]
+    cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 10)
+    cv2.line(image,
+             (cone.lower_right_bottom[0], cone.lower_right_bottom[1]),
+             (cone.lower_right_top[0], cone.lower_right_top[1]),
+             (0, 0, 255),
+             10)
+    # cv2.rectangle(image, cone.lower_left_bottom, cone.upper_right_top, (0, 0, 255), 10)
 
 
 print("---%s seconds---" % (time.time() - start_time))
--- cone.py
+++ cone.py
@@ -1,22 +1,107 @@
+import numpy as np
+
+margin = 10
 class Cone(object):
     def __init__(self):
         """generates an object where all necesarry attributes of cones are saved"""
-        self.top_box = []
-        self.top_con = []
-        self.bottom_box = []
-        self.bottom_con = []
-        self.distance = -1
-        self.left_bottom_corner = [0, 0]
-        self.right_top_corner = [0, 0]
+        self.upper_box = []
+        self.upper_con = []
+        self.lower_box = []
+        self.lower_con = []
+        # lower box
+        self.lower_left_bottom = [0, 0]
+        self.lower_right_bottom = [0, 0]
+        self.lower_right_top = [0, 0]
+        self.lower_left_top = [0, 0]
+        # upper box
+        self.upper_right_bottom = [0, 0]
+        self.upper_left_bottom = [0, 0]
+        self.upper_left_top = [0, 0]
+        self.upper_right_top = [0, 0]
+        # distance estimations
+        self.overall_height = 0
+        self.top_part_height = 0
+        self.ratio = 0
 
-    def rectangle(self):
-        print(self.top_con)
+    def upper_bounding_box(self):
+        """find the extrem points in all four directions of the upper contour"""
+        self.upper_right_bottom[0] = 0
+        self.upper_left_bottom[0] = 0
+        self.upper_left_top[0] = 0
+        self.upper_right_top[0] = 0
+        # top and bottom line where the extrem points should lie
+        line_top = self.upper_box[1]
+        line_bottom = self.upper_box[1] + self.upper_box[3]
+        for point in self.upper_con:
+            if line_top - margin < point[0][1] < line_top + margin:
+                # initalize points
+                if self.upper_left_top[0] == 0:
+                    self.upper_left_top = point[0]
+                    self.upper_right_top = point[0]
+                # detect left-top corner
+                if point[0][0] < self.upper_left_top[0]:
+                    self.upper_left_top = point[0]
+                # detect right top corner
+                if point[0][0] > self.upper_right_top[0]:
+                    self.upper_right_top = point[0]
+            if line_bottom - 3*margin < point[0][1] < line_bottom + 3*margin:
+                if self.upper_left_bottom[0] == 0:
+                    self.upper_left_bottom = point[0]
+                    self.upper_right_bottom = point[0]
+                if point[0][0] < self.upper_left_bottom[0]:
+                    self.upper_left_bottom = point[0]
+                if point[0][0] > self.upper_right_bottom[0]:
+                    self.upper_right_bottom = point[0]
+
+    def lower_bounding_box(self):
+        """find the extrem points in all four directions of the lower contour"""
+        self.lower_right_bottom[0] = 0
+        self.lower_left_bottom[0] = 0
+        self.lower_left_top[0] = 0
+        self.lower_right_top[0] = 0
+        # top and bottom line where the extrem points should lie
+        line_top = self.lower_box[1]
+        line_bottom = self.lower_box[1] + self.lower_box[3]
+        for point in self.lower_con:
+            if line_top - margin < point[0][1] < line_top + margin:
+                # initalize points
+                if self.lower_left_top[0] == 0:
+                    self.lower_left_top = point[0]
+                    self.lower_right_top = point[0]
+                # detect left-top corner
+                if point[0][0] < self.lower_left_top[0]:
+                    self.lower_left_top = point[0]
+                # detect right top corner
+                if point[0][0] > self.lower_right_top[0]:
+                    self.lower_right_top = point[0]
+            if line_bottom - 5 * margin < point[0][1] < line_bottom + 5 * margin:
+                if self.lower_left_bottom[0] == 0:
+                    self.lower_left_bottom = point[0]
+                    self.lower_right_bottom = point[0]
+                if point[0][0] < self.lower_left_bottom[0]:
+                    self.lower_left_bottom = point[0]
+                if point[0][0] > self.lower_right_bottom[0]:
+                    self.lower_right_bottom = point[0]
 
     def bounding_box(self):
-        box_x1 = self.bottom_box[0]
-        box_y1 = self.bottom_box[1] + self.bottom_box[3]
-        box_x2 = self.bottom_box[0] + self.bottom_box[2]
-        box_y2 = self.top_box[1]
-        self.left_bottom_corner = box_x1, box_y1
-        self.right_top_corner = box_x2, box_y2
+        """find the outermost points of the cone"""
+        box_x1 = self.lower_box[0]
+        box_y1 = self.lower_box[1] + self.lower_box[3]
+        box_x2 = self.lower_box[0] + self.lower_box[2]
+        box_y2 = self.upper_box[1]
+        self.lower_left_bottom = box_x1, box_y1
+        self.upper_right_top = box_x2, box_y2
         # return self.left_bottom_corner, self.right_top_corner
+
+    def distance(self):
+        """calculate the distance to the cone with different functions"""
+        # with overall height
+        height = self.lower_box[1] + self.lower_box[3] - self.upper_box[1]
+        self.overall_height = (2 * 708) / height    # calibrated for 2m distance
+        print("distance with overall height:   ", self.overall_height, "m")
+        # with side length
+        side = np.sqrt((self.lower_right_top[0] - self.upper_right_top[0]) ** 2
+                       + (self.lower_right_top[1] - self.upper_right_top[1]) ** 2)
+        self.top_part_height = (2 * 394) / side     # calibrated for 2m distance
+        print("distance with side length: %2.3f m" % self.top_part_height)
+        self.ratio = 0
